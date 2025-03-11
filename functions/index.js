5c89e945006e7bcef0102404ef4d549dd6c4e307
Fushinn <55939146+Xingsandesu@users.noreply.github.com>
2025-03-11 12:44:58 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,8 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const ipVersion = request.cf.ipVersion
   const redirectURL = ipVersion === 6
-    ? 'https://v6git.jintongshu.com'
-    : 'https://v4git.jintongshu.com'
+    ? 'https://v6git.jintongshu.com:8443'
+    : 'https://v4git.jintongshu.com:8000'
 
   return Response.redirect(redirectURL, 302)
 }
