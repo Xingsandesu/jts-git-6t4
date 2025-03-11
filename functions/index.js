b0fdb7c6c5bc8212b8e922034d731374242435e5
Fushinn <55939146+Xingsandesu@users.noreply.github.com>
2025-03-11 12:52:01 +0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,14 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const ipVersion = request.cf.ipVersion
-  const redirectURL = ipVersion === 6
-    ? 'https://v6git.jintongshu.com:8000'
-    : 'https://v4git.jintongshu.com:8443'
-
-  return Response.redirect(redirectURL, 302)
+export async function onRequest({ request, env, cf }) {
+  // 直接从 cf 对象获取 IP 版本
+  const ipVersion = cf.ipVersion;
+  
+  // 设置重定向目标
+  const ipv6Url = 'https://v6git.jintongshu.com:8000'; // 替换为您的 IPv6 目标网址
+  const ipv4Url = 'https://v4git.jintongshu.com:8443'; // 替换为您的 IPv4 目标网址
+  
+  // 根据 IP 版本决定重定向地址
+  const redirectUrl = ipVersion === 6 ? ipv6Url : ipv4Url;
+  
+  // 创建重定向响应
+  return Response.redirect(redirectUrl, 302);
 }
